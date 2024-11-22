4b5a4968ee4c8f93fa9aed72281e99cf62a89ebe
Marco Nicolini <marco.nicolini@merkle.com>
2024-11-22 00:04:17 -0300

    Template customization for POC

--- apps/main/app/layout.tsx
+++ apps/main/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <div className="text-center mt-5">
             <Image src="/images/merkle-logo.svg" alt="Merkle Inc." width={200} height={100} className="mx-auto"/>
             <h1 className="text-3xl text-bold my-5">Microfrontends POC (Main App)</h1>
-            <h2 className="text-xl text-center mb-5">Tech Stack: Vercel > Next.js with Multi-Zones</h2>
+            <h2 className="text-xl text-center mb-5">Tech Stack: Vercel &gt; Next.js with Multi-Zones</h2>
             <hr/>
         </div>
         {children}
