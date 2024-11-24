c65f9a8d528e19d5ef6864d5e7bf62452e14146d
Marco Nicolini <marco.nicolini@merkle.com>
2024-11-24 11:04:57 -0300

    Style update

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
         <html>
         <body>
         <div className="text-center mt-5">
-            <Image src="/images/merkle-logo.svg" alt="Merkle Inc." width={200} height={100} className="mx-auto"/>
+            <Image src="/images/merkle-logo.svg" alt="Merkle Inc." width={250} height={32} className="mx-auto"/>
             <h1 className="text-3xl text-bold my-5">Microfrontends POC (Kiosk App)</h1>
             <h2 className="text-xl text-center mb-5">Tech Stack: Vercel &gt; Next.js with Multi-Zones</h2>
             <hr/>
